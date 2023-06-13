fb76c5f715fe424b923f814dbd8dd2b0f3338226
JoaoChaves65 <joaomarcosgol2000@gmail.com>
2023-06-13 10:20:00 -0300

    Criando users.routes para as rotas do projeto

---
+++ src/routes/users.routes.ts
@@ -0,0 +1,10 @@
+import { Router } from 'express'
+class UsersRoutes {
+    private router: Router;
+    constructor(){
+        this.router = Router();
+    }
+    getRoutes() {
+        this.router.post('/users', )
+    }
+}
